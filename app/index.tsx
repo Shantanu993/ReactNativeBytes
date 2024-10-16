61c60079c55ceefd75d92167f768a80f175d0fc5
Shantanu993 <shantanugahtori4@gmail.com>
2024-10-16 15:24:29 +0530

    🎨 Views

--- app/index.tsx
+++ app/index.tsx
@@ -7,8 +7,25 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "plum",
       }}
     >
+      <View
+        style={{
+          backgroundColor: "lightblue",
+          padding: 10,
+          height: 200,
+          width: 200,
+        }}
+      ></View>
+      <View
+        style={{
+          backgroundColor: "lightgreen",
+          padding: 10,
+          height: 200,
+          width: 200,
+        }}
+      ></View>
       <Text>Hello World!</Text>
     </View>
   );
