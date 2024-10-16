7d979dce301741a1875b4068a4a1d5fd4e59dac2
Shantanu993 <shantanugahtori4@gmail.com>
2024-10-16 15:47:09 +0530

    🎨 ImageBackground

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, ImageBackground } from "react-native";
 // import LogoImg from "../assets/images/icon.png";
 const LogoImg = require("../assets/images/icon.png"); // require is used to import images
 
@@ -12,30 +12,41 @@ export default function Index() {
         backgroundColor: "plum",
       }}
     >
-      <Image
+      <ImageBackground
         source={LogoImg}
         style={{
-          height: 200,
-          width: 200,
+          flex: 1,
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
         }}
-      />
-      <Image
-        source={{ uri: "https://picsum.photos/200" }} // need to pass object with uri key for network images
-        style={{
-          height: 200,
-          width: 200,
-        }} // mandatory to set height and width
-      />
-      <View
-        style={{
-          backgroundColor: "lightgreen",
-          height: 200,
-          width: 200,
-        }}
-      ></View>
-      <Text>
-        Hello World! <Text style={{ color: "blue" }}>This is nested text</Text>
-      </Text>
+      >
+        {/* <Image
+          source={LogoImg}
+          style={{
+            height: 200,
+            width: 200,
+          }}
+        /> */}
+        <Image
+          source={{ uri: "https://picsum.photos/200" }} // need to pass object with uri key for network images
+          style={{
+            height: 200,
+            width: 200,
+          }} // mandatory to set height and width
+        />
+        <View
+          style={{
+            backgroundColor: "lightgreen",
+            height: 200,
+            width: 200,
+          }}
+        ></View>
+        <Text>
+          Hello World!{" "}
+          <Text style={{ color: "blue" }}>This is nested text</Text>
+        </Text>
+      </ImageBackground>
     </View>
   );
 }
