e0b81c23ef00ac00d44276c982f40d6b04fb2e58
Shantanu993 <shantanugahtori4@gmail.com>
2024-10-23 12:03:24 +0530

    🎨 Dimensions API

--- app/index.js
+++ app/index.js
@@ -1,8 +1,17 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Dimensions } from "react-native";
 
 export default function Index() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <Text style={styles.text}>Welcome!</Text>
+      </View>
+    </View>
+  );
 }
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 // key value pairs are used to define styles
 const styles = StyleSheet.create({
@@ -11,5 +20,16 @@ const styles = StyleSheet.create({
     backgroundColor: "plum",
     justifyContent: "center",
     alignItems: "center",
+  },
+  box: {
+    backgroundColor: "lightblue",
+    height: windowHeight > 600 ? "70%" : "90%",
+    width: windowWidth > 500 ? "60%" : "90%",
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: windowWidth > 500 ? "60" : "24",
   },
 });
