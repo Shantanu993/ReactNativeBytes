3b76933ef221dd922b7ac32cbe972939519d069a
Shantanu993 <shantanugahtori4@gmail.com>
2024-10-23 23:35:32 +0530

    ✨ Update app.json and index.js for default orientation and dynamic box dimensions using Dimensions API

--- app/index.js
+++ app/index.js
@@ -1,17 +1,44 @@
 import { Text, View, StyleSheet, Dimensions } from "react-native";
+import { useState, useEffect } from "react";
 
 export default function Index() {
+  const [dimensions, setDimensions] = useState({
+    window: Dimensions.get("window"),
+  });
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener("change", ({ window }) => {
+      setDimensions({ window });
+    });
+
+    return () => subscription?.remove();
+  });
+
+  const { window } = dimensions;
+  const windowWidth = window.width;
+  const windowHeight = window.height;
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome!</Text>
+      <View
+        style={[
+          styles.box,
+          {
+            height: windowHeight > 600 ? "70%" : "90%",
+            width: windowWidth > 500 ? "60%" : "90%",
+          },
+        ]}
+      >
+        <Text style={{ fontSize: windowWidth > 500 ? "60" : "24" }}>
+          Welcome!
+        </Text>
       </View>
     </View>
   );
 }
 
-const windowWidth = Dimensions.get("window").width;
-const windowHeight = Dimensions.get("window").height;
+// const windowWidth = Dimensions.get("window").width;
+// const windowHeight = Dimensions.get("window").height;
 
 // key value pairs are used to define styles
 const styles = StyleSheet.create({
@@ -23,13 +50,13 @@ const styles = StyleSheet.create({
   },
   box: {
     backgroundColor: "lightblue",
-    height: windowHeight > 600 ? "70%" : "90%",
-    width: windowWidth > 500 ? "60%" : "90%",
+    // height: windowHeight > 600 ? "70%" : "90%",
+    // width: windowWidth > 500 ? "60%" : "90%",
     borderRadius: 10,
     justifyContent: "center",
     alignItems: "center",
   },
   text: {
-    fontSize: windowWidth > 500 ? "60" : "24",
+    // fontSize: windowWidth > 500 ? "60" : "24",
   },
 });
