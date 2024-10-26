09092f6dcadef6ce9c3c3d4f5d71631c47930f16
Shantanu993 <shantanugahtori4@gmail.com>
2024-10-27 01:49:46 +0530

    add styling to card component

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,9 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 
 export default function PokemonCard() {
   return (
-    <View>
+    <View style={styles.card}>
       <Text>Pokemon Card</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 5,
+    borderWidth: 2,
+    margin: 16,
+    padding: 16,
+    ...Platform.select({
+      ios: {
+        shadowColor: "#333",
+        shadowOffset: { width: 2, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 4,
+      },
+    }),
+  },
+});
