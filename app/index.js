54a8ffc59b308dcee73ddfb4c994ae3610b02e6c
Shantanu993 <shantanugahtori4@gmail.com>
2024-10-31 14:15:09 +0530

    make get request and bind response to ui

--- app/index.js
+++ app/index.js
@@ -17,23 +17,42 @@ import {
 import { useState, useEffect } from "react";
 
 export default function Index() {
-  const [postList, setpostList] = useState([]);
+  const [postList, setPostList] = useState([]);
 
   const fetchData = async (limit = 10) => {
     const response = await fetch(
       `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
     );
     const data = await response.json();
-    setpostList(data);
+    setPostList(data);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
+  console.log(postList);
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.listContainer}></View>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={postList}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.card}>
+                <Text style={styles.titleText}>{item.title}</Text>
+                <Text style={styles.bodyText}>{item.body}</Text>
+              </View>
+            );
+          }}
+          ItemSeparatorComponent={<View style={{ height: 16 }} />}
+          ListEmptyComponent={<Text>No Posts Found</Text>}
+          ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
+          ListFooterComponent={
+            <Text style={styles.footerText}>End of list</Text>
+          }
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -48,5 +67,30 @@ const styles = StyleSheet.create({
   listContainer: {
     flex: 1,
     paddingHorizontal: 10,
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    padding: 10,
+    borderWidth: 1,
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  bodyText: {
+    fontSize: 16,
+    color: "#333",
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  footerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
