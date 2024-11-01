a893b51d8f7e77c0ab510b5ee7ad052c8781edce
Shantanu993 <shantanugahtori4@gmail.com>
2024-11-01 15:47:42 +0530

    add addPost function

--- app/index.js
+++ app/index.js
@@ -21,6 +21,9 @@ export default function Index() {
   const [postList, setPostList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(false);
+  const [isPosting, setIsPosting] = useState(false);
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
 
   const fetchData = async (limit = 10) => {
     const response = await fetch(
@@ -50,29 +53,71 @@ export default function Index() {
     );
   }
 
+  const addPost = async () => {
+    setIsPosting(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: postTitle,
+        body: postBody,
+      }),
+    });
+    const newPost = await response.json();
+    setPostList([newPost, ...postList]);
+    setPostTitle("");
+    setPostBody("");
+    setIsPosting(false);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.listContainer}>
-        <FlatList
-          data={postList}
-          renderItem={({ item }) => {
-            return (
-              <View style={styles.card}>
-                <Text style={styles.titleText}>{item.title}</Text>
-                <Text style={styles.bodyText}>{item.body}</Text>
-              </View>
-            );
-          }}
-          ItemSeparatorComponent={<View style={{ height: 16 }} />}
-          ListEmptyComponent={<Text>No Posts Found</Text>}
-          ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
-          ListFooterComponent={
-            <Text style={styles.footerText}>End of list</Text>
-          }
-          refreshing={refreshing}
-          onRefresh={handleRefresh}
-        />
-      </View>
+      <>
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="Post Title"
+            value={postTitle}
+            onChangeText={setPostTitle}
+            style={styles.input}
+          />
+          <TextInput
+            placeholder="Post Body"
+            value={postBody}
+            onChangeText={setPostBody}
+            style={styles.input}
+          />
+          <Button
+            onPress={addPost}
+            disabled={isPosting}
+            title={isPosting ? "Adding..." : "Add Post"}
+          />
+        </View>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={postList}
+            renderItem={({ item }) => {
+              return (
+                <View style={styles.card}>
+                  <Text style={styles.titleText}>{item.title}</Text>
+                  <Text style={styles.bodyText}>{item.body}</Text>
+                </View>
+              );
+            }}
+            ItemSeparatorComponent={<View style={{ height: 16 }} />}
+            ListEmptyComponent={<Text>No Posts Found</Text>}
+            ListHeaderComponent={
+              <Text style={styles.headerText}>Post List</Text>
+            }
+            ListFooterComponent={
+              <Text style={styles.footerText}>End of list</Text>
+            }
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+          />
+        </View>
+      </>
     </SafeAreaView>
   );
 }
@@ -119,5 +164,20 @@ const styles = StyleSheet.create({
     paddingTop: StatusBar.currentHeight,
     justifyContent: "center",
     alignItems: "center",
+  },
+  inputContainer: {
+    padding: 10,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    borderWidth: 1,
+    margin: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
   },
 });
