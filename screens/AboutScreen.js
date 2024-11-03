34fbbc7f71589d62d2488fde90f35e84960fa16f
Shantanu993 <shantanugahtori4@gmail.com>
2024-11-03 13:02:50 +0530

    use useNavigation hook in AboutScreen

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { useNavigation } from "expo-router";
 
 const AboutScreen = () => {
+  //using useNavigation hook
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
       <Text style={styles.text}>About Screen</Text>
+      <Button
+        title="Go to Home Screen"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
     </View>
   );
 };
