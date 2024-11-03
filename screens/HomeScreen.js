57f3527e63efb0be50b9788dcdeed345d2226902
Shantanu993 <shantanugahtori4@gmail.com>
2024-11-03 13:02:28 +0530

    use navigation prop in HomeScreen component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+//using navigation prop
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
